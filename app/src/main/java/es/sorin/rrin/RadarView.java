f42a8cd42512bd1952987dfce3bbdc9189565e28
Sorin <sorindra@ucm.es>
2021-05-25 23:10:40 +0200

    Improvements in the radar view.

--- app/src/main/java/es/sorin/rrin/RadarView.java
+++ app/src/main/java/es/sorin/rrin/RadarView.java
@@ -8,17 +8,20 @@ import android.view.View;
 
 public class RadarView extends View
 {
-    private Paint _red;
+    private final Paint _color;
+    private float _sweepAngle;
 
     public RadarView(Context context, AttributeSet attrs)
     {
         super(context, attrs);
 
-        _red = new Paint();
+        _color = new Paint();
 
-        _red.setColor(0xFFDD0000);
-        _red.setStyle(Paint.Style.STROKE);
-        _red.setStrokeWidth(10.0f);
+        _color.setColor(0xFF17d801);
+        _color.setStrokeWidth(10.0f);
+        _color.setAntiAlias(true);
+
+        _sweepAngle = 45;
     }
 
     @Override
@@ -29,12 +32,28 @@ public class RadarView extends View
         final float height = getHeight();
         final float halfHeight = height / 2.0f;
 
+        //Solid background.
         canvas.drawARGB(255, 5, 4, 4);
-        canvas.drawCircle(halfWidth, halfHeight, halfWidth - 20, _red);
-        canvas.drawCircle(halfWidth, halfHeight, halfWidth - 120, _red);
-        canvas.drawCircle(halfWidth, halfHeight, halfWidth - 220, _red);
-        canvas.drawPoint(halfWidth, halfHeight, _red);
-        canvas.drawLine(halfWidth, 100, halfWidth, height - 100, _red);
-        canvas.drawLine(0, halfHeight, width, halfHeight, _red);
+
+        //Radar.
+        _color.setStyle(Paint.Style.STROKE);
+        canvas.drawCircle(halfWidth, halfHeight, halfWidth - 20, _color);
+        canvas.drawCircle(halfWidth, halfHeight, halfWidth - 120, _color);
+        canvas.drawCircle(halfWidth, halfHeight, halfWidth - 220, _color);
+        canvas.drawLine(halfWidth, 100, halfWidth, halfHeight - 100, _color);
+        canvas.drawLine(halfWidth, halfHeight + 100, halfWidth, height - 100, _color);
+        canvas.drawLine(0, halfHeight, halfWidth - 100, halfHeight, _color);
+        canvas.drawLine(halfWidth + 100, halfHeight, width, halfHeight, _color);
+        _color.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(halfWidth, halfHeight, 20, _color);
+
+        //Sweep line.
+        final float x = halfWidth + (float)Math.cos(_sweepAngle) * halfWidth;
+        final float y = halfHeight - (float)Math.sin(_sweepAngle) * halfWidth;
+
+        canvas.drawLine(halfWidth, halfHeight, x, y, _color);
+
+        //Text.
+        //TODO
     }
 }
