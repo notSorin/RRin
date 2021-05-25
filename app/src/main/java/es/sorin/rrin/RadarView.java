acc9293b7efbf9c9cc42efff81a3bc288ffc5da2
Sorin <sorindra@ucm.es>
2021-05-25 22:27:28 +0200

    Created the radar view and added it to the main activity view.

---
+++ app/src/main/java/es/sorin/rrin/RadarView.java
@@ -0,0 +1,20 @@
+package es.sorin.rrin;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class RadarView extends View
+{
+    public RadarView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+
+    }
+}
