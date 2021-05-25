252e23a94af8867328f02aa4bf0e66eb1c9cc566
Sorin <sorindra@ucm.es>
2021-05-26 00:23:37 +0200

    Added an id to the radar view.

--- app/src/main/java/es/sorin/rrin/MainActivity.java
+++ app/src/main/java/es/sorin/rrin/MainActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity
 {
+    private RadarView _radarView;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _radarView = findViewById(R.id.Radar);
     }
 }
